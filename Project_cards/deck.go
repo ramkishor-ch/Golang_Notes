9e814893de1516fe81fbe9150a9380f24bd1a9e2
Ramkishor Chaladi <ramkishor.ch@Ramkishors-MacBook-Pro.local>
2023-05-30 19:55:32 +0530

    updated

--- Project_cards/deck.go
+++ Project_cards/deck.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	// declaring lists
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// If you don't want to use index then you can mention it like "_"
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
 
 // receiver argument is 'd'.
 func (d deck) print() {
@@ -17,5 +37,27 @@ func (d deck) print() {
 // in the function as a variable called 'd'
 // d: reference to cards variables
 // d: can be called as self or this in other languages.
-
 // deck: Every variable of type 'deck' can call this function on itself.
+
+// returning multiple values.
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
+}
+
+// byte slice : Ascii of user input string
+// []byte("Hi there!")
+//[]byte: Type we want
+//"Hi there!": Value we have
+// Example:
+// greeting := "Hi there!"
+// fmt.Println([]byte(greeting))
+
+// Deck to String and joining slice of strings
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// save data to harddrive or file
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
--- Project_cards/main.go
+++ Project_cards/main.go
@@ -1,9 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
 	// var: about to create new variable
 	// card: name of the variable
 	// string: string will be ever be assigned to this variable
@@ -18,28 +16,37 @@ func main() {
 	// int: 0, -10000, 99999
 	// float64: 10.1, 0.00009, -199.01
 
-	card = newCard()
-	fmt.Println(card)
+	cards := newDeck()
+	// fmt.Println(card)
 
-	//Array: Fixed length list of things
-	//Slice: An array that can grow or shrink.
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	// Array: Fixed length list of things
+	// Slice: An array that can grow or shrink.
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards = append(cards, "Six of Spades")
 	cards.print()
 
 	// for i, card := range cards {
 	// 	fmt.Println(i, card)
 	// }
-	//i: index of this element in the array
-	//card: Current card were iterating over
-	//range cards: Take the slice of cards and loop over it
+	// i: index of this element in the array
+	// card: Current card were iterating over
+	// range cards: Take the slice of cards and loop over it
 
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+
+	// Deck to String
+	card1 := newDeck()
+	// fmt.Println(card1.toString())
+	card1.saveToFile("my_cards") //my_cards is the filename
 }
 
 // Functions and Return Types
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
 
 // func
 // newCard() : define a function called 'newCard'
